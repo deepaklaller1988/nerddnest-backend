c87e63d2cef7737c048a6ae0b42c661baf5ab849
Dinesh <dlaller1988@gmail.com>
2024-11-26 15:30:25 +0530

    Changes in Forgot and signup 15:29 26-11-2024

--- src/db/config/config.ts
+++ src/db/config/config.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+dotenv.config({ path: `.env` });
 
 const options = {
     username: process.env.DB_USER,
